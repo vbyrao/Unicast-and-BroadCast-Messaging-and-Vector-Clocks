b5bc7a2474da602a99a33a5b3de948a5ac5a5cbb
vbyrao <vbyrao@gmail.com>
2024-05-28 20:41:58 -0500

    Broadcast/Unicast Socket Programming- Initail Commit

---
+++ broadcast/code2.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HOST = 'localhost'
+
+class Process:
+    def __init__(self):
+        self.id = 1
+        self.clock = [0] * 3
+
+    def broadcast(self):
+        while True:
+            broad_msg = input('Enter a message to broadcast: \n')
+            print(f'Before sending the message clock - {self.clock}\n')
+            self.clock[self.id] += 1
+            clk_at_msg = f'{broad_msg}:{self.clock}'.encode('utf-8')
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, 1234))
+            s.send(clk_at_msg)
+            s.close()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, 1236))
+            s.send(clk_at_msg)
+            s.close()
+
+    def receive(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, 1235))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                data = conn.recv(1024)
+                broad_msg = data.decode('utf-8')
+                clk_rcvd = [int(x) for x in broad_msg.split(':')[1][1:-1].split(',')]
+                for i in range(3):
+                    self.clock[i] = max(self.clock[i], clk_rcvd[i])
+                self.clock[self.id] = self.clock[self.id]+1
+                print(f'Received message "{broad_msg}" with clock {clk_rcvd} and updated own clock to {self.clock}\n')
+
+process = Process()
+threading.Thread(target=process.broadcast).start()
+threading.Thread(target=process.receive).start()
+
